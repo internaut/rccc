c0c1810de270ca97425361dacb5d64dd51939cc3
Markus Konrad <post@mkonrad.net>
2012-10-20 18:17:14 +0300

    value calculation is okay

--- js/resistor.js
+++ js/resistor.js
@@ -23,10 +23,11 @@ function bandClicked(event) {
         // set the active band number
         activeBand = clickedBand;
         
-        // show the color selector
+        // get the color selector
         var clrSelector = $('#clrSelector');
         clrSelector.hide();
         
+        // configure the color selector for the current band
         var i = 0;
         clrSelector.children().each(function() {
             $(this).show();
@@ -44,11 +45,46 @@ function bandClicked(event) {
             i++;
         });
         
+        // show the color selector
         $(this).append(clrSelector);
         clrSelector.show('fast');
     } else {
-        
+        closeClrSelector();
     }
+}
+
+function closeClrSelector() {
+    $('#clrSelector').hide('fast');
+    activeBand = 0;
+}
+
+function clrFieldOver(event) {
+    $(this).addClass('clrSelectorSelected');
+    
+    $('#resBand' + activeBand).css({backgroundColor: $(this).css('backgroundColor')
+        })
+    
+    updateValues(activeBand, $(this).index());
+}
+
+function clrFieldOut(event) {
+    $(this).removeClass('clrSelectorSelected');
+}
+
+function updateValues(band, val) {
+    // set the band's value
+    var bandValId = '#resBand' + band + 'Val';
+    $(bandValId).text(val);
+    
+    // calculate new ohms
+    var b = new Array();
+    for (var i = 1; i < 4; i++) {
+        b[i - 1] = parseFloat($('#resBand' + i + 'Val').text());
+    }
+    
+    var resVal = (b[0] * 10.0 + b[1]) * Math.pow(10, b[2]);
+    
+    $('#resValue').val(resVal);
 }
 
 $(document).ready(function(){
@@ -58,7 +94,11 @@ $(document).ready(function(){
     for (var i = 0; i < bandColors.length; ++i) {
         var c = bandColors[i];
         clrSelector.append('<div></div>');
-        $('#clrSelector > div:last').addClass('clrSelectorField').css({backgroundColor: c});
+        $('#clrSelector > div:last')
+            .addClass('clrSelectorField')
+            .css({backgroundColor: c})
+            .mouseover(clrFieldOver)
+            .mouseout(clrFieldOut);
     }
     
     $('#clrSelector > div:last').append('<em>none</em>');
